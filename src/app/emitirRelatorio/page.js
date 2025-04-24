0795458ac2dd62826c1ca7cb254e792e10623ac0
cecilioana <anapaulacecilioodeoliveira900@gmail.com>
2025-04-23 20:25:37 -0400

    resumo da tela

--- src/app/emitirRelatorio/page.js
+++ src/app/emitirRelatorio/page.js
@@ -9,7 +9,7 @@ export default function Relatorio() {
       <Header />
       <div className={styles.fundo}>
       <div className={styles.data}>
-        <h1 className={styles.h1}>Emitir relatório</h1>
+        <h2 className={styles.h2}>Emitir relatório</h2>
         <form>
           <div className={styles.venda}>
             <label className={styles.label} htmlFor="data-inicio">
@@ -19,52 +19,6 @@ export default function Relatorio() {
             <label htmlFor="data-fim" className={styles.label}>
               à
               <input className={styles.input} type="date" id="data-fim" name="data-fim" />
-            </label>
-          </div>
-          <div className={styles.entrega}>
-            <label className={styles.label} htmlFor="data-entrega1">
-              Data de entrega
-              <input className={styles.input} type="date" id="data-entrega1" name="data-entrega1" />
-            </label>
-            <label className={styles.label} htmlFor="data-entrega2">
-              à
-              <input className={styles.input} type="date" id="data-entrega2" name="data-entrega2" />
-            </label>
-          </div>
-          <div className={styles.produtoo}>
-            <label className={styles.label} htmlFor="produto">
-              Produto
-              <input className={styles.input}
-                type="text"
-                id="produto"
-                placeholder="Digite o nome do produto"
-                name="produto"
-                required
-              />
-            </label>
-          </div>
-          <div className={styles.vendedorr}>
-            <label className={styles.label} htmlFor="vendedor">
-              Vendedor
-              <input className={styles.input}
-                type="text"
-                id="vendedor"
-                placeholder="Digite o nome do vendedor"
-                name="vendedor"
-                required
-              />
-            </label>
-          </div>
-          <div className={styles.clientee}>
-            <label className={styles.label} htmlFor="cliente">
-              Cliente
-              <input className={styles.input}
-                type="text"
-                id="cliente"
-                placeholder="Digite o nome do cliente"
-                name="cliente"
-                required
-              />
             </label>
           </div>
         </form>
